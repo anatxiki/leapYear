4cf7c9bfb435b6d072bfe576da231758d5f2e5f9
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 10:41:36 +0200

    feat: 2019 no es bisiesto

--- test/leapYear.test.ts
+++ test/leapYear.test.ts
@@ -4,4 +4,8 @@ describe("un año no es bisiesto si no es divisible por 4", () => {
   it("2021 no es bisiesto", () => {
     expect(leapYear("2021")).toBeFalsy();
   });
+
+  it("2019 no es bisiesto", () => {
+    expect(leapYear("2019")).toBeFalsy();
+  });
 });
