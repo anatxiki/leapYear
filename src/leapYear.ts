45ba86ae63b07724329d377e22e7e66223b43545
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 13:07:19 +0200

    refactor: elimina funcion innecesaria

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,12 +1,8 @@
 export function leapYear(year: number): boolean {
-  if (isNotLeapYear(year)) {
-    return false;
-  }
-
-  return true;
+  return (
+    !(isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)) &&
+    isDivisibleBy(year, 4)
+  );
 }
 
 const isDivisibleBy = (year: number, divisor: number) => year % divisor === 0;
-const isNotLeapYear = (year: number) =>
-  (isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)) ||
-  !isDivisibleBy(year, 4);
