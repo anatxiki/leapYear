d3f29a3e80f9f1a212e7497d81aaf2ace7c2cf67
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 10:46:47 +0200

    fix:naming test

--- test/leapYear.test.ts
+++ test/leapYear.test.ts
@@ -5,7 +5,7 @@ describe("comprueba si un año es bisiesto", () => {
     expect(leapYear(2021)).toBeFalsy();
   });
 
-  it("2020 no es bisiesto", () => {
+  it("2020 es bisiesto", () => {
     expect(leapYear(2020)).toBeTruthy();
   });
 });
