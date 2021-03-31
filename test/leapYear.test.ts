a66f82eebca1b80f947e19a2b3665c360bb849bb
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 16:49:12 +0200

    refactor: test tobe true

--- test/leapYear.test.ts
+++ test/leapYear.test.ts
@@ -2,10 +2,10 @@ import { isLeapYear } from "../src/leapYear";
 
 describe("comprueba si un año es bisiesto", () => {
   it.each([[4], [8], [12], [400], [800], [1200]])("%d es bisiesto", (value) => {
-    expect(isLeapYear(value)).toBeTruthy();
+    expect(isLeapYear(value)).toBe(true);
   });
 
   it.each([[2], [100], [200], [500], [2100]])("%d no es bisiesto", (value) => {
-    expect(isLeapYear(value)).toBeFalsy();
+    expect(isLeapYear(value)).toBe(false);
   });
 });
