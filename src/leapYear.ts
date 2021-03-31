9bdcc99482175b719810ee73f18b31065c0e81ce
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 12:01:29 +0200

    refactor: generaliza

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,13 +1,5 @@
 export function leapYear(year: number): boolean {
-  if (year === 4) {
-    return true;
-  }
-
-  if (year === 8) {
-    return true;
-  }
-
-  if (year === 12) {
+  if (year % 4 === 0) {
     return true;
   }
   return false;
