80d0e4f630da789136c4667f5abe254c30c7670c
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 13:12:48 +0200

    refactor: elimina clausulas confusas

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,8 +1,7 @@
 export function leapYear(year: number): boolean {
-  return (
-    !(isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)) &&
-    isDivisibleBy(year, 4)
-  );
+  return isDivisibleBy(year, 100)
+    ? isDivisibleBy(year, 400)
+    : isDivisibleBy(year, 4);
 }
 
 const isDivisibleBy = (year: number, divisor: number) => year % divisor === 0;
