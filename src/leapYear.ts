191d53c9712f93ba781e957e0c7e5966a09249bc
calvo111979 <acalvoinfa10@gmail.com>
2021-03-31 11:59:11 +0200

    refactor: elimina tests innecesarios

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,8 +1,4 @@
 export function leapYear(year: number): boolean {
-  if (year === 2020) {
-    return true;
-  }
-
   if (year === 4) {
     return true;
   }
--- test/leapYear.test.ts
+++ test/leapYear.test.ts
@@ -1,14 +1,6 @@
 import { leapYear } from "../src/leapYear";
 
 describe("comprueba si un año es bisiesto", () => {
-  it("2021 no es bisiesto", () => {
-    expect(leapYear(2021)).toBeFalsy();
-  });
-
-  it("2020 es bisiesto", () => {
-    expect(leapYear(2020)).toBeTruthy();
-  });
-
   it("4 es bisiesto", () => {
     expect(leapYear(4)).toBeTruthy();
   });
